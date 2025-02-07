fc0b10b0930b551c6b9ac949f5056e7ba42bc699
Jayden Kim <hkim@lcs.on.ca>
2025-02-07 12:07:24 -0500

    Initial Commit

---
+++ LottiesAnimationsList/LottiesAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottiesAnimationsListApp.swift
+//  LottiesAnimationsList
+//
+//  Created by kim heejun on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LottiesAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
