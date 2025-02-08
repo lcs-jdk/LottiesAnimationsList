bbc82f912b91e8ecc628b5f1f9347d1c6d71f70a
Jayden Kim <hkim@lcs.on.ca>
2025-02-08 10:46:23 -0500

    Added several animations that I like.

---
+++ LottiesAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,8 @@
+//
+//  AnimationsListView.swift
+//  LottiesAnimationsList
+//
+//  Created by kim heejun on 2/7/25.
+//
+
+import Foundation
---
+++ LottiesAnimationsList/Views/LottiesView.swift
@@ -0,0 +1,54 @@
+//
+//  LottiesView.swift
+//  LottiesAnimationsList
+//
+//  Created by kim heejun on 2/8/25.
+//
+
+
+
+import UIKit
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+
+    @State var animationNamed: String
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        
+        let view = UIView(frame: .zero)
+        
+        // Lottie View
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(animationNamed)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        // Constraints
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+        
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    typealias UIViewType = UIView
+    
+    struct LottieView_Previews: PreviewProvider {
+        static var previews: some View {
+            LottieView(animationNamed: "782-check-mark-success")
+        }
+    }
+}
